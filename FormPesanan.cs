606bafbd64354a37df04b5ad26b657f58b617384
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2026-04-15 13:09:23 +0700

    penambahan fungsi Form Pesanan

--- FormPesanan.cs
+++ FormPesanan.cs
@@ -13,34 +13,108 @@ namespace EZTailor
 {
     public partial class FormPesanan: Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=LAPTOPAQA\\AQASAMA;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public FormPesanan()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
-        private void cbPelanggan_SelectedIndexChanged(object sender, EventArgs e)
+        void LoadPelanggan()
         {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT id_pelanggan, nama FROM Pelanggan", conn);
 
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            cbPelanggan.DataSource = dt;
+            cbPelanggan.DisplayMember = "nama";
+            cbPelanggan.ValueMember = "id_pelanggan";
         }
-
-        private void txtJenis_TextChanged(object sender, EventArgs e)
+        void TampilData()
         {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT p.id_pesanan, pl.nama, p.jenis_pakaian, p.tanggal_pesan, p.harga " +
+                "FROM Pesanan p JOIN Pelanggan pl ON p.id_pelanggan = pl.id_pelanggan",
+                conn);
 
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
         }
-
-        private void lblPelanggan_Click(object sender, EventArgs e)
+        private void FormPesanan_Load(object sender, EventArgs e)
         {
-
+            LoadPelanggan();
+            TampilData();
         }
-
-        private void lblJenis_Click(object sender, EventArgs e)
+        private void btnTambah_Click(object sender, EventArgs e)
         {
+            conn.Open();
 
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Pesanan VALUES(@id, @jenis, @tgl, @harga)", conn);
+
+            cmd.Parameters.AddWithValue("@id", cbPelanggan.SelectedValue);
+            cmd.Parameters.AddWithValue("@jenis", txtJenis.Text);
+            cmd.Parameters.AddWithValue("@tgl", dtTanggal.Value);
+            cmd.Parameters.AddWithValue("@harga", txtHarga.Text);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Pesanan berhasil ditambahkan");
+
+            TampilData();
         }
-
-        private void lblHarga_Click(object sender, EventArgs e)
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            cbPelanggan.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtJenis.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            dtTanggal.Value = Convert.ToDateTime(dataGridView1.CurrentRow.Cells[3].Value);
+            txtHarga.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            conn.Open();
 
+            SqlCommand cmd = new SqlCommand(
+                "UPDATE Pesanan SET id_pelanggan=@id, jenis_pakaian=@jenis, tanggal_pesan=@tgl, harga=@harga WHERE id_pesanan=@idpesanan",
+                conn);
+
+            cmd.Parameters.AddWithValue("@id", cbPelanggan.SelectedValue);
+            cmd.Parameters.AddWithValue("@jenis", txtJenis.Text);
+            cmd.Parameters.AddWithValue("@tgl", dtTanggal.Value);
+            cmd.Parameters.AddWithValue("@harga", txtHarga.Text);
+
+            cmd.Parameters.AddWithValue("@idpesanan",
+                dataGridView1.CurrentRow.Cells[0].Value.ToString());
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Pesanan berhasil diupdate");
+
+            TampilData();
+        }
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "DELETE FROM Pesanan WHERE id_pesanan=@id", conn);
+
+            cmd.Parameters.AddWithValue("@id",
+                dataGridView1.CurrentRow.Cells[0].Value.ToString());
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Pesanan berhasil dihapus");
+
+            TampilData();
         }
     }
 }
