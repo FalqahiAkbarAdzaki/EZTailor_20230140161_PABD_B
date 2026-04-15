c397869351a7cceb63e6b13a42a1aef85bd88181
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2026-04-15 12:46:45 +0700

    Add project files.

---
+++ FormPelanggan.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace EZTailor
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.txtNoHp = new System.Windows.Forms.TextBox();
+            this.lblNoHp = new System.Windows.Forms.Label();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.lblAlamat = new System.Windows.Forms.Label();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(180, 63);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(240, 22);
+            this.txtNama.TabIndex = 0;
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(44, 69);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(44, 16);
+            this.lblNama.TabIndex = 1;
+            this.lblNama.Text = "Nama";
+            // 
+            // txtNoHp
+            // 
+            this.txtNoHp.Location = new System.Drawing.Point(180, 109);
+            this.txtNoHp.Name = "txtNoHp";
+            this.txtNoHp.Size = new System.Drawing.Size(257, 22);
+            this.txtNoHp.TabIndex = 2;
+            // 
+            // lblNoHp
+            // 
+            this.lblNoHp.AutoSize = true;
+            this.lblNoHp.Location = new System.Drawing.Point(44, 115);
+            this.lblNoHp.Name = "lblNoHp";
+            this.lblNoHp.Size = new System.Drawing.Size(49, 16);
+            this.lblNoHp.TabIndex = 3;
+            this.lblNoHp.Text = "No. Hp";
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(180, 150);
+            this.txtAlamat.Multiline = true;
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(286, 149);
+            this.txtAlamat.TabIndex = 4;
+            // 
+            // lblAlamat
+            // 
+            this.lblAlamat.AutoSize = true;
+            this.lblAlamat.Location = new System.Drawing.Point(44, 150);
+            this.lblAlamat.Name = "lblAlamat";
+            this.lblAlamat.Size = new System.Drawing.Size(49, 16);
+            this.lblAlamat.TabIndex = 5;
+            this.lblAlamat.Text = "Alamat";
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(581, 62);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 23);
+            this.btnTambah.TabIndex = 6;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(581, 108);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Edit";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(581, 150);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 8;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(180, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(476, 115);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.lblAlamat);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.lblNoHp);
+            this.Controls.Add(this.txtNoHp);
+            this.Controls.Add(this.lblNama);
+            this.Controls.Add(this.txtNama);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.TextBox txtNoHp;
+        private System.Windows.Forms.Label lblNoHp;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.Label lblAlamat;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
